b05c630b7532714e1c51d8fe8283dc47cf45ab1e
PavelM <myagkiypavel@gmail.com>
2023-07-27 12:40:53 +0300

    feature: add Menu component

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,60 @@
+import {
+  BoxProps,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { menu } from "../../data/data";
+
+const listStyle: BoxProps["sx"] = {
+  "& .MuiListItemButton-root": {
+    px: { xs: 1, sm: 2 },
+  },
+  "& .MuiListItemIcon-root": {
+    minWidth: 0,
+  },
+  "& a": {
+    color: "inherit",
+    textDecoration: "none",
+  },
+};
+
+export const Menu = () => {
+  return (
+    <>
+      {menu.map(({ id, title, listItems }) => (
+        <List
+          key={id}
+          component="nav"
+          subheader={
+            <ListSubheader
+              component="div"
+              sx={{
+                display: { xs: "none", md: "block" },
+                textTransform: "uppercase",
+              }}
+            >
+              {title}
+            </ListSubheader>
+          }
+          sx={listStyle}
+        >
+          {listItems.map(({ id, title, url, icon }) => (
+            <Link to={url} key={id}>
+              <ListItemButton>
+                <ListItemIcon sx={{ mr: 1 }}>{icon}</ListItemIcon>
+                <ListItemText
+                  primary={title}
+                  sx={{ display: { xs: "none", md: "block" } }}
+                />
+              </ListItemButton>
+            </Link>
+          ))}
+        </List>
+      ))}
+    </>
+  );
+};
