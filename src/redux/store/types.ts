62f4244614f8b476054944edd7c912529803d831
PavelM <myagkiypavel@gmail.com>
2023-07-27 14:25:52 +0300

    feature: add redux store

---
+++ src/redux/store/types.ts
@@ -0,0 +1,7 @@
+import { Action, ThunkAction } from '@reduxjs/toolkit';
+
+import {store} from './store'
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, undefined, Action<string>>;
