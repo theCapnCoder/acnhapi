95fd20fd57846408b4fdd5d0e0f1c2ecff6b5924
PavelM <myagkiypavel@gmail.com>
2023-07-30 20:20:58 +0300

    type: add Fossisl

---
+++ src/redux/reducers/fossilsReducer/type.ts
@@ -0,0 +1,11 @@
+export type Fossils = {
+  id?: string;
+  "file-name": string;
+  name: {
+    [key: string]: string;
+  };
+  price: number;
+  "museum-phrase": string;
+  image_uri: string;
+}
+
