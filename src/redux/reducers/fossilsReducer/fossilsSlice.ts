1a76a08921969595eb3c5d0176976ef792eb460a
PavelM <myagkiypavel@gmail.com>
2023-07-30 20:20:47 +0300

    feature: fossils slice

---
+++ src/redux/reducers/fossilsReducer/fossilsSlice.ts
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { AxiosError } from "axios";
+import { Fossils } from "./type";
+import { getAllFossils } from "./actionCreators/getAllFossils";
+
+type InitialState = {
+  fossils: { [fossilName: string]: Fossils };
+  status: 'loading' | "success" | 'failed',
+  error: string | undefined,
+}
+
+const initialState: InitialState = {
+  fossils: {},
+  status: 'loading',
+  error: undefined
+}
+
+const fossilsSlice = createSlice({
+  name: "fossils",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getAllFossils.pending, (state) => {
+        state.status = 'loading'
+      })
+      .addCase(getAllFossils.fulfilled, (state, action) => {
+        state.status = 'success'
+        state.fossils = action.payload
+      })
+      .addCase(getAllFossils.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = (action.payload as AxiosError).message
+      })
+  }
+
+})
+
+export default fossilsSlice.reducer;
