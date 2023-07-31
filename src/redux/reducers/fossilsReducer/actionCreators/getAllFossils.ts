79f2831b9e9a86d9b5627967ea84e78819688a00
PavelM <myagkiypavel@gmail.com>
2023-07-31 20:30:25 +0300

    chore: get all fossils

--- src/redux/reducers/fossilsReducer/actionCreators/getAllFossils.ts
+++ src/redux/reducers/fossilsReducer/actionCreators/getAllFossils.ts
@@ -1,11 +1,10 @@
-import { createAsyncThunk, nanoid } from "@reduxjs/toolkit";
-import { Fossils } from "../type";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const getAllFossils = createAsyncThunk(
   'fossils/getFossils',
   async (_, thunkAPI) => {
     try {
-      const response = await fetch(`https://acnhapi.com/v1/fossils`)
+      const response = await fetch(`https://acnhapi.com/v1/villagers`)
       const data = await response.json()
 
       return data
