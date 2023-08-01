04a096d712e7c078df43dd64074215e8c97d6901
PavelM <myagkiypavel@gmail.com>
2023-08-01 18:44:42 +0300

    chore: combineReducer

--- src/redux/store/combinreReducer.ts
+++ src/redux/store/combinreReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import fishReducer from '../reducers/fishReducer/fishSlice'
 import fossilsReducer from '../reducers/fossilsReducer/fossilsSlice'
+import villagersSlice from '../reducers/villagersReducer/villagersSlice';
 
 
 export const combinedReducer = combineReducers({
   fish: fishReducer,
   fossils: fossilsReducer,
+  villagers: villagersSlice,
 });
